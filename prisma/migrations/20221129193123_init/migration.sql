492e64be3a7bea00fa2a70f785a7595a878e7aea
shahidul1004 <sonysust@gmail.com>
2022-12-21 18:04:00 +0600

    changes

---
+++ prisma/migrations/20221129193123_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "authorId" INTEGER NOT NULL,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { trpc } from "../utils/trpc";
 
 export default function IndexPage() {
-  const { data: users } = trpc.useQuery(["user.findManyUser", {}]);
+  const { data: users } = trpc.useQuery(["user.findManyUser", {include: {posts: {}}}]);
 
   const { data: user } = trpc.useQuery([
     "user.findUniqueUser",
@@ -10,29 +10,56 @@ export default function IndexPage() {
 
   const { data: posts } = trpc.useQuery(["post.findManyPost", {}]);
 
+  const {mutate: createUser} = trpc.useMutation(["user.createOneUser"])
+  const {mutate: createPost} = trpc.useMutation(["post.createOnePost"])
+
+  const { mutate : updateUser} = trpc.useMutation(["user.updateOneUser"])
+
   const { isLoading, error, mutate } = trpc.useMutation(["post.createOnePost"]);
+
+  const {data: groupUser} = trpc.useQuery(["user.groupByUser", {by: ["id"], orderBy: {_min: {name: "asc"}}}])
 
   console.log({ users }, { posts });
   console.log(user);
+  console.log(groupUser);
 
   return (
     <div>
       <button
         onClick={async () => {
-          mutate({
-            data: {
-              author: {
-                connect: {
-                  id: 1,
-                },
-              },
-              title: "p1",
-              content: "content1",
-            },
-          });
+          createUser({
+            data: {name: "sony"}
+          })
         }}
       >
         add user
+      </button>
+      <button
+        onClick={async () => {
+          createPost({
+            data: {title: "post", author: {connect: {id: 1}}}
+          })
+        }}
+      >
+        add post
+      </button>
+      <button
+        onClick={async () => {
+          updateUser({
+            where: {id: 1},
+            data: {name: "sony yy", posts: {
+              deleteMany: {id: 5},
+              upsert: {
+                where: {id: 10},
+                create: {title: "new upsert"},
+                update: {title: "updated upsert"}
+              }
+              
+            }}
+          })
+        }}
+      >
+        update user
       </button>
     </div>
   );
